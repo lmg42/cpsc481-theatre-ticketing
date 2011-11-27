5c1c2228e443c20242892720a4649d2c1427b4a3
lisa.graham42@gmail.com <lisa.graham42@gmail.com@5f0792c6-7c9a-5a9a-a0ef-90f5ec83c2ab>
2011-11-27 19:44:34 +0000

    Changed concert list to a tree view. Added concert details to persist concert data as concerts are changed.

---
+++ TheatreTicketing/ConcertDetails.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheatreTicketing
+{
+    enum ConcertSeries { Celebration, Discovery, Jazz, Organ, StringQuartet };
+    enum Concert
+    {
+        BlackbirdSings, HyphenatedLizst, SchubertWinterreise, LiteraryLiszt, OldVsNew,
+        ContemporaryCanada, HistoireDuTango, PersianZeal,
+        TommyBanks, BillEvans, MariaSchneider, ChrisAndrew, Verismo, GoodmanMahQuartet,
+        GermanySeduced, FrenchSymphonic, LivreDOrgue,
+        BrahmsI, BrahmsII, BrahmsIII
+    };
+    enum SeatType { Empty, Adult, StudentSenior, UofCStudent };
+
+    class ConcertDetails
+    {
+        ConcertSeries seriesName;
+        Concert concertName;
+        List<Seats> seatingChart;
+
+        class Seats
+        {
+            string seatCode;
+            SeatType seatTypeValue;
+            bool seatPurchased;
+        }
+    }
+}
